79524419f3091227a7845e07241d8bd0c9f3e261
Joseline Velasquez <joselinev534@gmail.com>
2020-04-22 11:30:54 -0700

    updated

--- converting_windows_to_vcf.sh
+++ converting_windows_to_vcf.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-#python script location 
-py_script=/home/jvelasquez2408/Desktop/actualproject_harddrive/spring_2020/Code/SummerGenomics/VCFtoFASTA.py
-py_script2=/home/jvelasquez2408/Desktop/actualproject_harddrive/spring_2020/Code/fasta2nexus.py
-for f in ../*.vcf; do
+#python script location
+py_script=/home/joseline/Desktop/cattle/actualproject/Code/SummerGenomics/VCFtoFASTA.py
+py_script2=/home/joseline/Desktop/cattle/actualproject/Code/JoselineMVR/fasta2nexus.py
+metafi=/home/joseline/Desktop/cattle/actualproject/spring_2020/vcfs/meta_og_nd.csv
+window_size=50000
+
+for f in *.vcf; do
 	ofi=$(echo `basename $f` | cut -d '.' -f1)
 	opath=".fasta"
-	python2 ${py_script} $f $ofi$opath
-done 
+	python2 ${py_script} $f $ofi$opath $metafi
+done
 
-for d in *.fasta; do 
+for d in *.fasta; do
 	ofi2=$(echo `basename $d` | cut -d '.' -f1)
 	opath2=".nex"
 	python2 $py_script2 $d $ofi2$opath2
-done 
+done
+
+mv *.vcf vcf/
+mv *.fasta fasta/
+mv *.nex nexus/
