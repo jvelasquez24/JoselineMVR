50341586aa0b8aeda6a5fec9ca15fd40eabe4de4
jvelasquez24 <joselinev534@gmail.com>
2021-04-08 13:13:35 -0700

    fixed

--- creatingwindows.py
+++ creatingwindows.py
@@ -1,25 +1,40 @@
 #creating windows 
 import sys 
+import math
 #inputs 
 infile = sys.argv[1]
-outfile = sys.argv[2]
-window_size = sys.argv[3]
-#Getting header & data
-lines_per_file = int(window_size)
-smallfile = None
-header=''
-with open(infile) as bigfile:
-    for lineno, line in enumerate(bigfile):
-    	if line.startswith('#'):
-    		header=line
-        if lineno % lines_per_file == 0:
-            if smallfile:
-                smallfile.close()
-            small_filename = str(outfile)+'{}.vcf'.format(lineno)
-            smallfile = open(small_filename, "w")
-            smallfile.write(header)
-    	if '#' in line:
-    		continue 
-        smallfile.write(line)
-    if smallfile:
-        smallfile.close()
+window_size = int(sys.argv[2])
+name = infile.split(".")[0]
+#Settinng position markers 
+start_pos = 0
+end_pos = start_pos + window_size
+header = " "
+#opening vcf & header 
+inputvcf = open(infile,"r")
+for line in inputvcf:
+	if line.startswith("#CHROM"):
+		header = line
+inputvcf.close() 
+
+#creating windows 
+inputvcf = open(infile,"r")
+ofi = open(name + "_{}.vcf".format(start_pos), 'w')
+ofi.write(header)
+
+for basecall in inputvcf:
+	if basecall.startswith("#"):
+			continue 
+	pos = int(basecall.split('\t')[1])
+	if start_pos < pos < end_pos:
+		ofi.write(basecall)
+	elif pos >= end_pos:
+		ofi.close()
+		start_pos = math.floor(pos / window_size) * window_size
+		print("{} is geater than {}".format(pos, end_pos))
+		end_pos = start_pos + window_size
+		file_name = name + "_{}.vcf".format(start_pos)
+		print("starting a new file named {}, for bases {} to {}".format(file_name, start_pos, end_pos))
+		ofi = open(file_name, 'w')
+		ofi.write(header)
+		end_pos = start_pos + window_size
+		ofi.write(basecall)
